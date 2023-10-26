a3df7a97fac91b35bd52c6c1800d38fda0957e3f
joao-zac <102718142+joao-zac@users.noreply.github.com>
2023-10-26 12:53:47 -0300

    💄 Style: Tornando responsivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import api from './services/api'
 import { useEffect, useState } from 'react'
+import DividerDesktop from '../images/pattern-divider-desktop.svg'
+import DividerMobile from '../images/pattern-divider-mobile.svg'
 
 function App() {
   const [user, setUser]:any = useState({});
@@ -21,11 +23,13 @@ function App() {
   return (
     <>
       <main>
-        <span>{user.id}</span> 
+        <span>#{user.id}</span> 
         
         <p>{user.advice}</p> 
         
-        <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+        <img src={DividerDesktop} className='DDesktop' />
+        <img src={DividerMobile} className='DMobile'/>
+
         <button onClick={handleFetch}><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg></button>
       </main>
     </>
